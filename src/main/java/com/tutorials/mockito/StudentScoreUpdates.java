82940fcc26c74be54047265ce158367c845ad0ed
RachidSAOUDI <102986947+RachidSAOUDI@users.noreply.github.com>
2023-08-15 14:55:09 +0100

    Create StudentScoreUpdates.java

---
+++ src/main/java/com/tutorials/mockito/StudentScoreUpdates.java
@@ -0,0 +1,19 @@
+public class StudentScoreUpdates {
+  public IDatabase databaseImpl; 
+   
+  public StudentScoreUpdates(IDatabase databaseImpl)
+  {
+    this.databaseImpl = databaseImpl;
+  } 
+   
+  public void calculateSumAndStore(String studentId, int[] scores)
+  {
+    int total = 0;
+    for(int score : scores)
+  {
+    total = total + score;
+  }
+    // write total to DB
+    databaseImpl.updateScores(studentId, total);
+  }
+}
